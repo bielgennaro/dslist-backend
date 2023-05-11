e1e361b22a26af7d5ebcfe99c380ddb69025d16a
bielgennaro <zirondigabriel@gmail.com>
2023-05-11 14:53:53 -0300

    Project Initialized

---
+++ src/test/java/com/bielgennaro/dslist/DsListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bielgennaro.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
