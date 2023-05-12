f420fe7dc9aca1d14b2e8000e7698cef13455bf9
bielgennaro <zirondigabriel@gmail.com>
2023-05-11 23:06:14 -0300

    Implementado: Seeds no database

--- src/main/java/com/bielgennaro/dslist/entities/Game.java
+++ src/main/java/com/bielgennaro/dslist/entities/Game.java
@@ -15,21 +15,26 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
-    private String shortDescription;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
+    private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
+
 
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String shortDescription,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String shortDescription,
                 String imgUrl, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
+        this.score = score;
         this.shortDescription = shortDescription;
         this.imgUrl = imgUrl;
         this.longDescription = longDescription;
@@ -67,12 +72,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public String getShortDescription() {
@@ -97,6 +102,14 @@ public class Game {
 
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     @Override
